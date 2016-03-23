e51e3858e1cd4896c4a60c08114de51d43ccab45
shermanash <shermanash@gmail.com>
2016-03-23 22:02:40 +0000

    updates

--- proj_elastic.py
+++ proj_elastic.py
@@ -12,21 +12,24 @@ def InsertProj():
 			"numpos" : {"type": "string"},
 			"name" : {"type": "string", "index": "not_analyzed"},
 			"dk_sal" : {"type": "float"},
+			"Team" : {"type": "string"},
 			"Start" : {"type": "boolean"},
 			"DK_Proj": {"type": "float"},
 			"value" : {"type": "float"},
 			"ceiling": {"type": "float"},
-			"min_7d_avg": {"type": "float"},
+			"min_proj": {"type": "float"},
 			"dk_per_min": {"type": "float"},
 			"home": {"type": "string"},
 			"opppts_avg": {"type": "float"},
+			"status" : {"type": "string", "index": "not_analyzed"},
 			"timestamp": {"type": "date"}
 				}
 			}
 		}
 
     es = Elasticsearch()
-    es.indices.create("projections")
+    es.indices.delete(index="projections", ignore=[400, 404])
+    es.indices.create("projections", ignore=400)
     es.indices.put_mapping(index="projections", doc_type="dataframe", body=mapping)
     date = datetime.today()
     time = datetime.utcnow()
@@ -38,14 +41,16 @@ def InsertProj():
 		 body = { "numpos" : row['numpos'],
 			  "name" : row['name'],
 			  "dk_sal" : row['dk_sal'],
+			  "Team" : row['Team'],
 			  "Start" : row['Start'],
 			  "DK_Proj" : row['DK_Proj'],
 			  "value" : row['value'],
 			  "ceiling" : row['ceiling'],
-			  "min_7d_avg" : row['min_7d_avg'],
+			  "min_proj" : row['min_proj'],
 			  "dk_per_min" : row['dk_per_min'],
 			  "home" : row['home'],
 			  "opppts_avg" : row['opppts_avg'],
+			  "status" : row['status'],
 			  "timestamp": time})
 		
 
