0a10f83d00f80e887e733c687d4cc1090480291f
shermanash <shermanash@gmail.com>
2016-04-07 20:19:29 +0000

    fixed memory leak by calling optimize as subprocess

--- dflask/dflaskr.py
+++ dflask/dflaskr.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import pandas as pd
 import numpy as np
 import pickle
@@ -6,6 +7,37 @@ import sys
 sys.path.append('/home/ubuntu/dfsharp/')
 
 from optimizer_openopt import optimizer
+
+
+import os, cPickle
+
+
+def run_in_separate_process(func, *args, **kwds):
+    pread, pwrite = os.pipe()
+    pid = os.fork()
+    if pid > 0:
+        os.close(pwrite)
+        with os.fdopen(pread, 'rb') as f:
+            status, result = cPickle.load(f)
+        os.waitpid(pid, 0)
+        if status == 0:
+            return result
+        else:
+            raise result
+    else: 
+        os.close(pread)
+        try:
+            result = func(*args, **kwds)
+            status = 0
+        except Exception, exc:
+            result = exc
+            status = 1
+        with os.fdopen(pwrite, 'wb') as f:
+            try:
+                cPickle.dump((status,result), f, cPickle.HIGHEST_PROTOCOL)
+            except cPickle.PicklingError, exc:
+                cPickle.dump((2,exc), f, cPickle.HIGHEST_PROTOCOL)
+        os._exit(0)
 
 
 app = Flask(__name__)
@@ -28,7 +60,8 @@ def home_page():
 
 	delta = request.form['slider']
 	try:
-	    result = optimizer(locks=locks, exclusions=excludes, delta=int(delta))
+	    result = run_in_separate_process(optimizer, locks=locks, exclusions=excludes, delta=int(delta))
+	    #result = optimizer(locks=locks, exclusions=excludes, delta=int(delta))
             return render_template('layout.html', data=result)
 	    #return render_template('layout.html', data=["<--- Your lineup!"])
 	except:
